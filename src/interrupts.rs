68ce9d5a72ec694f0206cc436a4f457d2fe33549
leo <leo.henches@gmail.com>
2024-01-26 16:26:41 +0100

    finished double fault exceptions

--- src/interrupts.rs
+++ src/interrupts.rs
@@ -1,4 +1,6 @@
+use crate::gdt;
 use crate::println;
+use crate::serial_println;
 use lazy_static::lazy_static;
 use x86_64::structures::idt::{InterruptDescriptorTable, InterruptStackFrame};
 
@@ -6,13 +8,21 @@ lazy_static! {
     static ref IDT: InterruptDescriptorTable = {
         let mut idt = InterruptDescriptorTable::new();
         idt.breakpoint.set_handler_fn(breakpoint_handler);
-        idt.double_fault.set_handler_fn(double_fault_handler);
+        unsafe {
+            idt.double_fault
+                .set_handler_fn(double_fault_handler)
+                .set_stack_index(gdt::DOUBLE_FAULT_IST_INDEX);
+        }
         idt
     };
 }
 
 pub fn init_idt() {
     IDT.load()
+}
+
+pub fn test_init_gdb() {
+    TEST_IDT.load()
 }
 
 extern "x86-interrupt" fn breakpoint_handler(stack_frame: InterruptStackFrame) {
---
+++ tests/stack_overflow.rs
@@ -0,0 +1,60 @@
+#![no_std]
+#![no_main]
+#![feature(abi_x86_interrupt)]
+
+use core::panic::PanicInfo;
+use lazy_static::lazy_static;
+
+use x86_64::structures::idt::{InterruptDescriptorTable, InterruptStackFrame};
+
+use rust_os::interrupts::test_init_gdb;
+use rust_os::serial_print;
+use rust_os::serial_println;
+
+use rust_os::exit_qemu;
+use rust_os::QemuExitCode;
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    serial_print!("Testing double fault exception...\t");
+
+    rust_os::gdt::init();
+    test_init_gdb();
+    // rust_os::init();
+
+    stack_overflow();
+    // panic!("Execution continued after stack overflow");
+    loop {}
+}
+
+#[allow(unconditional_recursion)]
+fn stack_overflow() {
+    stack_overflow();
+    volatile::Volatile::new(0).read();
+}
+
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    rust_os::test_panic_handler(info);
+}
+lazy_static! {
+    static ref TEST_IDT: InterruptDescriptorTable = {
+        let mut idt = InterruptDescriptorTable::new();
+        unsafe {
+            idt.double_fault
+                .set_handler_fn(test_double_fault_handler)
+                .set_stack_index(rust_os::gdt::DOUBLE_FAULT_IST_INDEX);
+        }
+        idt
+    };
+}
+
+extern "x86-interrupt" fn test_double_fault_handler(
+    _stack_frame: InterruptStackFrame,
+    _error_code: u64,
+) -> ! {
+    serial_println!("[ok]");
+    exit_qemu(QemuExitCode::Success);
+    loop {}
+}
