7e7bc4a530f92b94c6830f9b9468ca2364a1f562
Léo Henches <leo.henches@gmail.com>
2024-01-27 18:28:22 +0100

    updated main

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,10 @@
 #![no_main]
 #![feature(custom_test_frameworks)]
 #![test_runner(rust_os::test_runner)]
-#![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
 
-use rust_os::println;
+use rust_os::{println, test_panic_handler};
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
@@ -14,36 +13,11 @@ pub extern "C" fn _start() -> ! {
 
     rust_os::init();
 
-    x86_64::instructions::interrupts::int3();
-
-    fn stack_overflow() {
-        stack_overflow();
-    }
-
-    stack_overflow();
-
-    // trigger a page fault
-    // unsafe {
-    //     *(0xdeadbeef as *mut u8) = 42;
-    // }
-
-    #[cfg(test)]
-    test_main();
-
     println!("Did not crash");
-
     loop {}
 }
 
-#[cfg(not(test))]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
-    println!("{}", info);
-    loop {}
-}
-
-#[cfg(test)]
-#[panic_handler]
-fn panic(info: &PanicInfo) -> ! {
-    rust_os::test_panic_handler(info)
+    test_panic_handler(info);
 }
